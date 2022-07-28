2d002f6404774cfb1c53a30871072ef50c01a24b
Admin <2919681828@qq.com>
2022-07-28 22:09:40 +0800

    添加地址簿的新增功能，设为默认地址功能，地址簿列表功能

---
+++ src/main/java/com/guanghe/takeout/mapper/AddressBookMapper.java
@@ -0,0 +1,9 @@
+package com.guanghe.takeout.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.guanghe.takeout.entity.AddressBook;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AddressBookMapper extends BaseMapper<AddressBook> {
+}
