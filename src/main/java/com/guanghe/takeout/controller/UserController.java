f08e0d3d24359c595a7941f62c0d537d572a4ab6
Admin <2919681828@qq.com>
2022-07-28 21:38:36 +0800

    添加通过验证码登录操作，没有调用发送验证码api，直接输入验证码进行登录

--- src/main/java/com/guanghe/takeout/controller/UserController.java
+++ src/main/java/com/guanghe/takeout/controller/UserController.java
@@ -1,10 +1,22 @@
 package com.guanghe.takeout.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.guanghe.takeout.common.R;
+import com.guanghe.takeout.entity.User;
 import com.guanghe.takeout.service.UserService;
+import com.guanghe.takeout.utils.SMSUtils;
+import com.guanghe.takeout.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -14,4 +26,68 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /**
+     * 发送手机短信验证码
+     * @param user
+     * @return
+     */
+    @PostMapping("/sendMsg")
+    public R<String> sendMsg(@RequestBody User user, HttpSession session){
+        //获取手机号
+        String phone = user.getPhone();
+
+        if (!StringUtils.isEmpty(phone)){
+            //生成随机的4为验证码
+            String code = ValidateCodeUtils.generateValidateCode(4).toString();
+            log.info("code = {}",code);
+
+            //调用阿里云短信服务API完成发送短信服务
+            //SMSUtils.sendMessage("签名","",phone,code);
+
+            //需要将生成的验证码保存到Session中
+            session.setAttribute("phone",code);
+            return R.success("手机验证码短信发送成功");
+
+        }
+
+        return R.error("短信发送失败");
+    }
+
+    /**
+     * 移动端用户登录
+     * @param map
+     * @param session
+     * @return
+     */
+    @PostMapping("/login")
+    public R<User> login(@RequestBody Map map, HttpSession session){
+        log.info("用户登录：{}",map.toString());
+        //获取手机号
+        String phone = map.get("phone").toString();
+
+        //获取验证码
+        String code = map.get("code").toString();
+
+        //从session中获取保存的验证码
+        Object codeInSession = session.getAttribute("phone");
+
+        //进行验证码比对
+        if (codeInSession != null && codeInSession.equals(code)){
+            //比对成功，登录成功
+
+            LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(User::getPhone,phone);
+            User user = userService.getOne(queryWrapper);
+            if (user == null){//判断登录用户是否为新用户，是的话自动完成注册
+                user = new User();
+                user.setPhone(phone);
+                user.setStatus(1);
+                userService.save(user);
+            }
+            session.setAttribute("user",user.getId());
+            return R.success(user);
+        }
+
+        return R.error("登录失败");
+    }
 }
